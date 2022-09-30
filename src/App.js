23491063adc90185253380a59bb28e8a277f20ac
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-30 22:32:26 +0900

    Feat: react-async를 이용해서 컴포넌트에 async/await 적용시키기 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import { db, getRdmCodes } from './Firebase';
+import { useAsync } from 'react-async';
 
-function App() {
-
-  const codeList = getRdmCodes(db);
-  console.log(codeList);
-
-  return (
-    <div className="App">
-      <p>hello</p>
-    </div>
-  );
+const loadRdmCodes = async () => {
+  const codeList = await getRdmCodes(db);
+  return codeList;
 }
+
+const App = () => {
+  const { data, error } = useAsync({ promiseFn: loadRdmCodes, customerId: 1 });
+  if (error) return `Something went wrong: ${error.message}`
+  if (data)
+    console.log(data);
+  
+    return (
+      <div className="App">
+        <p>hello</p>
+      </div>
+    );
+};
 
 export default App;
